f6065c2abcb2c29edb8e269b22a35d876d8d2921
sanjibame <jobservice.sanjib@gmail.com>
2026-07-27 18:46:22 +0530

    Add configuration settings for screen, robot, and colors

---
+++ config.py
@@ -0,0 +1,18 @@
+# Screen Size
+
+WIDTH = 640
+HEIGHT = 480
+
+# Robot
+
+ROBOT_SPEED = 5
+
+# Colors
+
+GREEN = (0,255,0)
+RED = (255,0,0)
+BLUE = (255,255,255)
+
+# Face
+
+FACE_COLOR = (255,255,0)
